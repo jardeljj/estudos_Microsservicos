3c77c5831ab0888baed08d09ff257c84b702adff
Jardel Junior <55859324+jardeljj@users.noreply.github.com>
2025-05-07 14:41:14 -0300

    fix.

--- inventory-service/src/main/java/br/com/microservices/orchestrated/inventoryservice/core/dto/OrderProducts.java
+++ inventory-service/src/main/java/br/com/microservices/orchestrated/inventoryservice/core/dto/OrderProducts.java
@@ -1,6 +1,6 @@
 package br.com.microservices.orchestrated.inventoryservice.core.dto;
 
-import br.com.microservices.orchestrated.orchestratorservice.core.dto.Product;
+
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
